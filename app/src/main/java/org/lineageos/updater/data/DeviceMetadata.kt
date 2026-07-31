crdroidandroid/android_packages_apps_Updater

Updater: Add device metadata info and links
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

app/src/main/java/org/lineageos/updater/data/DeviceMetadata.kt
@@ -1,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.updater.data
+
+data class DeviceMetadata(
+    val maintainer: String? = null,
+    val forum: String? = null,
+    val telegram: String = DEFAULT_TELEGRAM,
+    val paypal: String = DEFAULT_PAYPAL,
+) {
+    companion object {
+        const val DEFAULT_TELEGRAM = "https://t.me/crDroidAndroid"
+        const val DEFAULT_PAYPAL = "https://crdroid.net/donate"
+    }
+}

SenpaiSource/luna_packages_apps_Updater

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

app/src/main/java/org/lineageos/updater/data/DeviceMetadata.kt
@@ -1,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.updater.data
+
+data class DeviceMetadata(
+    val maintainer: String? = null,
+    val forum: String? = null,
+    val telegram: String = DEFAULT_TELEGRAM,
+    val paypal: String = DEFAULT_PAYPAL,
+) {
+    companion object {
+        const val DEFAULT_TELEGRAM = "https://t.me/lunaris_hub"
+        const val DEFAULT_PAYPAL = "https://paypal.me/ghosuto007"
+    }
+}